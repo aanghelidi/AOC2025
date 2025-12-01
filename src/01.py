11e8ebf17147530948c0c529c9a7a8089ec45659
aanghelidi <alainanghelidi@gmail.com>
2025-12-01 18:24:03 +0100

    refactor: remove unused dict

--- src/01.py
+++ src/01.py
@@ -1,5 +1,4 @@
 import sys
-from collections import defaultdict
 
 
 def count_extra(current_pos: int, rotation_char: str, distance: int) -> int:
@@ -16,14 +15,14 @@ with open(sys.argv[1]) as f:
     data = f.read().splitlines()
 
 current = 50
-positions = defaultdict(int)
+ans = 0
 ans2 = 0
 for line in data:
     rotation, distance = line[0], int(line[1:])
     ans2 += count_extra(current, rotation, distance)
     coef = 1 if rotation == "R" else -1
     current = (current + distance * coef) % 100
-    positions[current] += 1
+    ans += current == 0
 
-print(f"Part 1: {positions[0]}")
+print(f"Part 1: {ans}")
 print(f"Part 2: {ans2}")
